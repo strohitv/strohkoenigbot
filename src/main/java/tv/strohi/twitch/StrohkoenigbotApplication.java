28edf18189916391e48cf93483011cdd1d03a284
marco <goethe@marco-fuchs.de>
2021-09-01 18:51:52 +0200

    change the application to a spring boot app

---
+++ src/main/java/tv/strohi/twitch/StrohkoenigbotApplication.java
@@ -0,0 +1,13 @@
+package tv.strohi.twitch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrohkoenigbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrohkoenigbotApplication.class, args);
+	}
+
+}
