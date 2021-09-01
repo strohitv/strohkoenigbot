4f87b92e70f1f5f32e3d3bba9b9a25aa78635553
marco <goethe@marco-fuchs.de>
2021-09-01 11:43:16 +0200

    add first version of unicode replacement table creation algorithm

--- src/main/java/tv/strohi/twitch/App.java
+++ src/main/java/tv/strohi/twitch/App.java
@@ -11,7 +11,6 @@ import java.math.BigInteger;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * Hello world!
@@ -27,5 +26,109 @@ public class App
     }
 
     private void run() {
+        buildCharacterReplacementTable();
+        System.out.println("");
+    }
+
+    private void buildCharacterReplacementTable(){
+        String[] asciiChars = getCharactersOfCharset();
+        List<String[]> yetUnknownReplacements = new ArrayList<>();
+
+        InputStream in = getClass().getResourceAsStream("/confusables-short.txt");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+        List<String> lines = new ArrayList<>();
+        try {
+            while(reader.ready()) {
+                lines.add(reader.readLine());
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        for (String line : lines) {
+            String[] reps = line.split(";");
+            String initialCharacter = getDecodedUtf8String(reps[0]);
+            String replacementResult = getDecodedUtf8String(reps[1]).replace("(", "").replace(")", "");
+
+            if (replacementResult.chars().mapToObj(c -> new String(new char[] {(char)c}))
+                    .allMatch(str -> Arrays.asList(asciiChars).contains(str))) {
+                System.out.println("Add: " + initialCharacter + " -> " + replacementResult);
+                replacementTable.put(initialCharacter, replacementResult);
+            } else {
+                yetUnknownReplacements.add(new String[] {initialCharacter, replacementResult});
+            }
+        }
+
+        int happened;
+        do {
+            happened = 0;
+
+            for (int i = 0; i < yetUnknownReplacements.size(); i++) {
+                if (replacementTable.containsKey(yetUnknownReplacements.get(i)[1])) {
+                    System.out.println("Add: " + yetUnknownReplacements.get(i)[0] + " -> " + yetUnknownReplacements.get(i)[1]);
+                    replacementTable.put(yetUnknownReplacements.get(i)[0], replacementTable.get(yetUnknownReplacements.get(i)[1]));
+
+                    yetUnknownReplacements.remove(i);
+                    i--;
+
+                    happened++;
+                }
+            }
+        } while(happened > 0);
+
+        for (String[] unknown : yetUnknownReplacements) {
+            System.out.println("Add: " + unknown[0] + " -> " + unknown[1]);
+            replacementTable.put(unknown[0], "");
+        }
+    }
+
+    private String getDecodedUtf8String(String encoded) {
+        List<Byte> bytes = new ArrayList<>();
+
+        String[] parts = encoded.split(" ");
+        for (String part : parts) {
+            try {
+                String fixedpart = part.replaceAll("\uFEFF", "").trim();
+                if (fixedpart.length() % 2 != 0) {
+                    fixedpart = "0" + fixedpart;
+                }
+
+                byte[] decoded = Hex.decodeHex(fixedpart);
+                for (byte b : decoded) {
+                    bytes.add(b);
+                }
+            } catch (DecoderException e) {
+                e.printStackTrace();
+            }
+        }
+
+        while (bytes.size() > 0 && bytes.get(0) == 0) {
+            bytes.remove(0);
+        }
+
+        byte[] result = new byte[bytes.size()];
+        for (int i = 0; i < bytes.size(); i++) {
+            if (bytes.get(i) != 0) {
+                result[i] = bytes.get(i);
+            }
+        }
+
+        return new String(result, StandardCharsets.UTF_8);
+    }
+
+    private String[] getCharactersOfCharset() {
+        Charset charset = StandardCharsets.US_ASCII;
+        SortedMap<BigInteger, String> charsInEncodedOrder = new TreeMap<>();
+        for (int i = 32; i < Character.MAX_VALUE; i++) {
+            String s = Character.toString((char) i);
+            byte[] encoded = s.getBytes(charset);
+            String decoded = new String(encoded, charset);
+            if (s.equals(decoded)) {
+                charsInEncodedOrder.put(new BigInteger(1, encoded), s);
+            }
+        }
+
+        return charsInEncodedOrder.values().toArray(String[]::new);
     }
 }
