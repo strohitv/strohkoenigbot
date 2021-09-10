10e3a14ef4cdd8fba9809ee0417b82f42a9bdd47
marco <goethe@marco-fuchs.de>
2021-09-10 10:58:09 +0200

    do first steps of splatoon 2 api authentication access

--- src/main/java/tv/strohi/twitch/strohkoenigbot/StrohkoenigbotApplication.java
+++ src/main/java/tv/strohi/twitch/strohkoenigbot/StrohkoenigbotApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tv.strohi.twitch.strohkoenigbot.chatbot.TwitchChatBot;
+import tv.strohi.twitch.strohkoenigbot.splatoonapi.Authentication;
 
 @SpringBootApplication
 public class StrohkoenigbotApplication {
@@ -17,6 +18,9 @@ public class StrohkoenigbotApplication {
 	}
 
 	public static void main(String[] args) {
+		String authUrl = new Authentication().buildAuthUrl().toString();
+		System.out.println(authUrl);
+
 		SpringApplication.run(StrohkoenigbotApplication.class, args);
 	}
 
