aeec9f4d68639da2edc336779151e64d93185e11
marco <goethe@marco-fuchs.de>
2021-10-12 20:17:16 +0200

    create update shell script to use on my raspberry pi

---
+++ refresh-strohkoenigbot.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+cd $SCRIPTPATH || (echo "cd to SCRIPTPATH failed. SCRIPTPATH: $SCRIPTPATH" && exit)
+
+# git operations
+git fetch
+git pull
+
+# maven rebuild and restart application
+mvn -U clean package spring-boot:run
