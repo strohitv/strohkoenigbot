cf2df65fd500804d6ccb994a42a332637129f2ed
marco <goethe@marco-fuchs.de>
2021-10-14 01:15:04 +0200

    write splatnet shop offers into twitch chat

--- src/main/java/tv/strohi/twitch/strohkoenigbot/splatoonapi/splatnet/SplatNetStoreWatcher.java
+++ src/main/java/tv/strohi/twitch/strohkoenigbot/splatoonapi/splatnet/SplatNetStoreWatcher.java
@@ -3,8 +3,13 @@ package tv.strohi.twitch.strohkoenigbot.splatoonapi.splatnet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import tv.strohi.twitch.strohkoenigbot.chatbot.spring.TwitchMessageSender;
 import tv.strohi.twitch.strohkoenigbot.splatoonapi.model.SplatoonMerchandises;
 import tv.strohi.twitch.strohkoenigbot.splatoonapi.utils.RequestSender;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 @Component
 public class SplatNetStoreWatcher {
@@ -14,6 +19,13 @@ public class SplatNetStoreWatcher {
 	public void setShopLoader(RequestSender shopLoader) {
 		this.shopLoader = shopLoader;
 	}
+	
+	private TwitchMessageSender sender;
+
+	@Autowired
+	public void setSender(TwitchMessageSender sender) {
+		this.sender = sender;
+	}
 
 	// todo: check user role
 	// todo: read gear from splatnet api
@@ -21,9 +33,22 @@ public class SplatNetStoreWatcher {
 	// todo: rate limiting for pns (extra class)
 	// todo: https://dev.twitch.tv/docs/irc/guide
 
-	@Scheduled(cron = "1 * * * * *")
+	// 10 seconds after each full hour
+	@Scheduled(cron = "10 0 * * * *")
 	public void refreshSplatNetShop() {
-		SplatoonMerchandises test = shopLoader.querySplatoonApi("/api/onlineshop/merchandises", SplatoonMerchandises.class);
-		System.out.println(test);
+		SplatoonMerchandises gearOffers = shopLoader.querySplatoonApi("/api/onlineshop/merchandises", SplatoonMerchandises.class);
+		System.out.println(gearOffers);
+		
+		if (gearOffers != null && gearOffers.getMerchandises() != null) {
+			if (gearOffers.getMerchandises().length >= 1 && gearOffers.getMerchandises()[0].getEndTime().isBefore(Instant.now().plus(1, ChronoUnit.HOURS))) {
+				SplatoonMerchandises.SplatoonMerchandise gear = gearOffers.getMerchandises()[0];
+				sender.send("strohkoenig", String.format("%d MINUTES LEFT FOR THIS GEAR IN SPLATNET GEAR SHOP: %s - brand: %s - \"%s\" - price: %d coins - main ability: %s - favored ability: %s - unlocked sub slots: %d", Duration.between(Instant.now(), gear.getEndTime()).abs().toMinutes() + 1, gear.getKind(), gear.getGear().getBrand().getName(), gear.getGear().getName(), gear.getPrice(), gear.getSkill().getName(), gear.getGear().getBrand().getFrequent_skill().getName(), gear.getGear().getRarity()));
+			}
+
+			if (gearOffers.getMerchandises().length > 1 && gearOffers.getMerchandises()[gearOffers.getMerchandises().length - 1].getEndTime().isAfter(Instant.now().plus(11, ChronoUnit.HOURS))) {
+				SplatoonMerchandises.SplatoonMerchandise gear = gearOffers.getMerchandises()[gearOffers.getMerchandises().length - 1];
+				sender.send("strohkoenig", String.format("NEW GEAR ARRIVED IN SPLATNET GEAR SHOP: %s - brand: %s - \"%s\" - price: %d coins - main ability: %s - favored ability: %s - unlocked sub slots: %d. Available for %d hours.", gear.getKind(), gear.getGear().getBrand().getName(), gear.getGear().getName(), gear.getPrice(), gear.getSkill().getName(), gear.getGear().getBrand().getFrequent_skill().getName(), gear.getGear().getRarity(), Duration.between(Instant.now(), gear.getEndTime()).abs().toHours() + 1));
+			}
+		}
 	}
 }
