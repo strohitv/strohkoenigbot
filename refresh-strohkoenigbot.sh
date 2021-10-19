c0605e17e8e52ef453948da616d401565fb38dc9
marco <goethe@marco-fuchs.de>
2021-10-19 21:26:12 +0200

    never end in restart sh script

--- refresh-strohkoenigbot.sh
+++ refresh-strohkoenigbot.sh
@@ -4,9 +4,14 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 
 cd $SCRIPTPATH || (echo "cd to SCRIPTPATH failed. SCRIPTPATH: '$SCRIPTPATH'" && exit)
 
-# git operations
-git fetch
-git pull
+while true
+do
+  # git operations
+  git fetch
+  git pull
 
-# maven rebuild and restart application
-mvn -U clean package spring-boot:run
+  # maven rebuild and restart application
+  mvn -U clean package spring-boot:run
+
+  sleep 1000
+done
