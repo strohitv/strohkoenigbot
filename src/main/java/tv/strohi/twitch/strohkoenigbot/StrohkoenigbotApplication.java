c8d552a0c118947ca0582730a9c8ff12bce4dbe9
marco <goethe@marco-fuchs.de>
2021-10-20 21:19:46 +0200

    send a status discord message at 4:53 am

--- src/main/java/tv/strohi/twitch/strohkoenigbot/StrohkoenigbotApplication.java
+++ src/main/java/tv/strohi/twitch/strohkoenigbot/StrohkoenigbotApplication.java
@@ -10,6 +10,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import tv.strohi.twitch.strohkoenigbot.chatbot.spring.DiscordBot;
 
 import java.util.Arrays;
 import java.util.List;
@@ -21,8 +22,6 @@ public class StrohkoenigbotApplication {
 	private static final Logger logger = LogManager.getLogger(StrohkoenigbotApplication.class.getSimpleName());
 	private static List<String> arguments = null;
 
-	// 1 day - 10 seconds
-	private static final long APPLICATION_LIFETIME = 86400000L - 10000;
 	private static ConfigurableApplicationContext app = null;
 
 	private HikariDataSource dataSource;
@@ -32,12 +31,18 @@ public class StrohkoenigbotApplication {
 		this.dataSource = dataSource;
 	}
 
+	private DiscordBot discordBot;
+
+	@Autowired
+	public void setDiscordBot(DiscordBot discordBot) {
+		this.discordBot = discordBot;
+	}
+
 	public static void main(String[] args) {
 		arguments = Arrays.stream(args).collect(Collectors.toUnmodifiableList());
 		app = SpringApplication.run(StrohkoenigbotApplication.class, args);
 	}
 
-//	@Scheduled(fixedRate = APPLICATION_LIFETIME, initialDelay = APPLICATION_LIFETIME)
 	@Scheduled(cron = "0 43 4 * * *")
 	public void shutdown() {
 		logger.info("restarting application");
@@ -51,6 +56,12 @@ public class StrohkoenigbotApplication {
 		} else {
 			System.exit(0);
 		}
+	}
+
+	@Scheduled(cron = "0 53 4 * * *")
+	public void sendHello() {
+		logger.info("sending hello message to strohkoenig");
+		discordBot.sendPrivateMessage(discordBot.loadUserIdFromDiscordServer("strohkoenig#8058"), "Bot is started and ready to go!");
 	}
 
 	@Bean
