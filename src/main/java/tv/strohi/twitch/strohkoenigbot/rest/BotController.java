e55825e33e40fe17e89d461af023ea9cc167215a
marco <goethe@marco-fuchs.de>
2022-02-09 14:46:58 +0100

    fix broken request mapping for bot status page

--- src/main/java/tv/strohi/twitch/strohkoenigbot/rest/BotController.java
+++ src/main/java/tv/strohi/twitch/strohkoenigbot/rest/BotController.java
@@ -3,11 +3,13 @@ package tv.strohi.twitch.strohkoenigbot.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tv.strohi.twitch.strohkoenigbot.rest.model.BotStatus;
 import tv.strohi.twitch.strohkoenigbot.splatoonapi.results.ResultsExporter;
 
-@RestController("/bot")
+@RestController
+@RequestMapping("/bot")
 public class BotController {
 	private ResultsExporter resultsExporter;
 
