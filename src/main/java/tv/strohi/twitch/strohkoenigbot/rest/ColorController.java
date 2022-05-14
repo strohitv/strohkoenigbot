92aa4e842f95d48ec9ca93e77b3fac10b7699f0f
marco <goethe@marco-fuchs.de>
2022-05-14 23:18:20 +0200

    try to use isavailable instead of a scheduled blocked event

--- src/main/java/tv/strohi/twitch/strohkoenigbot/rest/ColorController.java
+++ src/main/java/tv/strohi/twitch/strohkoenigbot/rest/ColorController.java
@@ -1,18 +1,13 @@
 package tv.strohi.twitch.strohkoenigbot.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tv.strohi.twitch.strohkoenigbot.chatbot.spring.DiscordBot;
 import tv.strohi.twitch.strohkoenigbot.rest.model.ColorBody;
 import tv.strohi.twitch.strohkoenigbot.utils.DiscordChannelDecisionMaker;
 import tv.strohi.twitch.strohkoenigbot.utils.SplatoonMatchColorComponent;
 
 import java.awt.*;
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 
 @RestController
@@ -33,7 +28,7 @@ public class ColorController {
 	}
 
 	@PostMapping
-	public Long setColors(@RequestBody ColorBody colors) {
+	public boolean setColors(@RequestBody ColorBody colors) {
 		discordBot.sendServerMessageWithImages(DiscordChannelDecisionMaker.getDebugChannelName(), String.format("attempting to set colors to: %s", colors));
 		if (colors.getOwnTeamColor() == null
 				|| colors.getOwnTeamColor().length < 3
@@ -42,25 +37,28 @@ public class ColorController {
 				|| colors.getOtherTeamColor().length < 3
 				|| Arrays.stream(colors.getOtherTeamColor()).filter(c -> c < 0 || c > 255).count() > 0) {
 			discordBot.sendServerMessageWithImages(DiscordChannelDecisionMaker.getDebugChannelName(), "received invalid color arrays");
-			return Instant.now().minus(1, ChronoUnit.MINUTES).getEpochSecond();
+			return splatoonMatchColorComponent.isAvailable();
 		}
 
 
 		Color ownTeamColor = new Color(colors.getOwnTeamColor()[0], colors.getOwnTeamColor()[1], colors.getOwnTeamColor()[2]);
 		Color otherTeamColor = new Color(colors.getOtherTeamColor()[0], colors.getOtherTeamColor()[1], colors.getOtherTeamColor()[2]);
 
-		splatoonMatchColorComponent.setBackgroundColor(ownTeamColor);
-		splatoonMatchColorComponent.setGreenColor(ownTeamColor);
-		splatoonMatchColorComponent.setRedColor(otherTeamColor);
+		splatoonMatchColorComponent.setColors(ownTeamColor, ownTeamColor, otherTeamColor);
 
 		discordBot.sendServerMessageWithImages(DiscordChannelDecisionMaker.getDebugChannelName(), "successfully updated colors");
 
-		return splatoonMatchColorComponent.getBlockedUntil().getEpochSecond();
+		return splatoonMatchColorComponent.isAvailable();
 	}
 
 	@PostMapping("reset")
 	public void resetColors() {
 		discordBot.sendServerMessageWithImages(DiscordChannelDecisionMaker.getDebugChannelName(), "resetting colors");
 		splatoonMatchColorComponent.reset();
+	}
+
+	@GetMapping("active")
+	public boolean isActive() {
+		return splatoonMatchColorComponent.isAvailable();
 	}
 }
--- src/main/java/tv/strohi/twitch/strohkoenigbot/utils/SplatoonMatchColorComponent.java
+++ src/main/java/tv/strohi/twitch/strohkoenigbot/utils/SplatoonMatchColorComponent.java
@@ -4,13 +4,11 @@ import lombok.Getter;
 import org.springframework.stereotype.Component;
 
 import java.awt.*;
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
 
 @Component
 @Getter
 public class SplatoonMatchColorComponent {
-	private Instant blockedUntil = Instant.now();
+	private boolean isAvailable = true;
 
 	private final Color backgroundStandardColor = new Color(187, 187, 187);
 	private final Color greenStandardColor = new Color(136, 255, 136);
@@ -25,40 +23,32 @@ public class SplatoonMatchColorComponent {
 	}
 
 	public void reset() {
-		blockedUntil = Instant.now().plus(60, ChronoUnit.SECONDS);
+		isAvailable = true;
 
 		backgroundColor = backgroundStandardColor;
 		greenColor = greenStandardColor;
 		redColor = redStandardColor;
 	}
 
-	public Color getBackgroundColor() {
-		return backgroundColor;
+	public void setColors(Color backgroundColor, Color greenColor, Color redColor) {
+		if (isAvailable) {
+			this.backgroundColor = backgroundColor;
+			this.greenColor = greenColor;
+			this.redColor = redColor;
+
+			isAvailable = false;
+		}
 	}
 
-	public void setBackgroundColor(Color backgroundColor) {
-		if (this.backgroundColor == backgroundStandardColor && Instant.now().isAfter(blockedUntil)) {
-			this.backgroundColor = backgroundColor;
-		}
+	public Color getBackgroundColor() {
+		return backgroundColor;
 	}
 
 	public Color getGreenColor() {
 		return greenColor;
 	}
 
-	public void setGreenColor(Color greenColor) {
-		if (this.greenColor == greenStandardColor && Instant.now().isAfter(blockedUntil)) {
-			this.greenColor = greenColor;
-		}
-	}
-
 	public Color getRedColor() {
 		return redColor;
-	}
-
-	public void setRedColor(Color redColor) {
-		if (this.redColor == redStandardColor && Instant.now().isAfter(blockedUntil)) {
-			this.redColor = redColor;
-		}
 	}
 }
