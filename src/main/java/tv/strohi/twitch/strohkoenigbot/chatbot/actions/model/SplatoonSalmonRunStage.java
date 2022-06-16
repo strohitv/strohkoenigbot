a2a6f15fe11557df4c76afd9d892b3f1df72761b
marco <goethe@marco-fuchs.de>
2022-06-17 00:15:46 +0200

    add salmon run stage enum

---
+++ src/main/java/tv/strohi/twitch/strohkoenigbot/chatbot/actions/model/SplatoonSalmonRunStage.java
@@ -0,0 +1,26 @@
+package tv.strohi.twitch.strohkoenigbot.chatbot.actions.model;
+
+import lombok.Getter;
+
+import java.util.EnumSet;
+
+@Getter
+public enum SplatoonSalmonRunStage {
+	SpawningGrounds(1, "Spawning Grounds", "Spawning", "Grounds", "sg"),
+	MaroonersBay(2, "Marooner's Bay", "Marooners Bay", "Marooner's", "Marooners", "Bay", "mb"),
+	LostOutpost(4, "Lost Outpost", "Lost", "Outpost", "lo"),
+	SalmonidSmokeyard(8, "Salmonid Smokeyard", "Salmonid", "Smokeyard", "ss"),
+	RuinsOfArkPolaris(16, "Ruins of Ark Polaris", "Ruins", "Ark Polaris", "Ark", "Polaris", "roap", "rap", "ra", "ap");
+
+	private final int flag;
+	private final String name;
+	private final String[] altNames;
+
+	SplatoonSalmonRunStage(int flag, String name, String... altNames) {
+		this.flag = flag;
+		this.name = name;
+		this.altNames = altNames;
+	}
+
+	public static final EnumSet<SplatoonSalmonRunStage> All = EnumSet.allOf(SplatoonSalmonRunStage.class);
+}
