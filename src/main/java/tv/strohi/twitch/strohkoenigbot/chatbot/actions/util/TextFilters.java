86ae9604f281ecb694a9bafb95197aaebb279f74
strohitv <the.real.strohi@gmail.com>
2022-06-19 13:08:49 +0200

    add text filters for salmon run stages and salmon run weapons

--- src/main/java/tv/strohi/twitch/strohkoenigbot/chatbot/actions/util/TextFilters.java
+++ src/main/java/tv/strohi/twitch/strohkoenigbot/chatbot/actions/util/TextFilters.java
@@ -2,9 +2,7 @@ package tv.strohi.twitch.strohkoenigbot.chatbot.actions.util;
 
 import lombok.Getter;
 import org.springframework.stereotype.Component;
-import tv.strohi.twitch.strohkoenigbot.chatbot.actions.model.DayFilter;
-import tv.strohi.twitch.strohkoenigbot.chatbot.actions.model.RuleFilter;
-import tv.strohi.twitch.strohkoenigbot.chatbot.actions.model.SplatoonStage;
+import tv.strohi.twitch.strohkoenigbot.chatbot.actions.model.*;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -19,6 +17,10 @@ public class TextFilters {
 	private final Map<RuleFilter, Pattern> ruleFilters = new HashMap<>();
 	private final Map<DayFilter, Pattern> dayWithTimeFilters = new HashMap<>();
 	private final Map<DayFilter, Pattern> dayWithoutTimeFilters = new HashMap<>();
+	private final Map<SplatoonSalmonRunWeapon, Pattern> salmonRunWeaponExcludeFilters = new HashMap<>();
+	private final Map<SplatoonSalmonRunWeapon, Pattern> salmonRunWeaponIncludeFilters = new HashMap<>();
+	private final Map<SplatoonSalmonRunStage, Pattern> salmonRunStageExcludeFilters = new HashMap<>();
+	private final Map<SplatoonSalmonRunStage, Pattern> salmonRunStageIncludeFilters = new HashMap<>();
 
 	private final Pattern timeFilter;
 
@@ -57,6 +59,30 @@ public class TextFilters {
 
 			String withoutTimeRegex = String.format("((?<=\\s)|^)(%s)((?=\\s)|$)", dayNames);
 			dayWithoutTimeFilters.put(dayFilter, Pattern.compile(withoutTimeRegex));
+		}
+
+		for (SplatoonSalmonRunWeapon weapon : SplatoonSalmonRunWeapon.All) {
+			StringBuilder weaponConcatenateBuilder = new StringBuilder(weapon.getName().toLowerCase());
+			Arrays.stream(weapon.getAltNames()).forEach(name -> weaponConcatenateBuilder.append("|").append(name.toLowerCase()));
+			String weaponNames = weaponConcatenateBuilder.toString();
+
+			String excludeRegex = String.format("((?<=\\s)|^)not\\s+(%s)((?=\\s)|$)", weaponNames);
+			salmonRunWeaponExcludeFilters.put(weapon, Pattern.compile(excludeRegex));
+
+			String includeRegex = String.format("((?<=\\s)|^)(%s)((?=\\s)|$)", weaponNames);
+			salmonRunWeaponIncludeFilters.put(weapon, Pattern.compile(includeRegex));
+		}
+
+		for (SplatoonSalmonRunStage stage : SplatoonSalmonRunStage.All) {
+			StringBuilder stageConcatenateBuilder = new StringBuilder(stage.getName().toLowerCase());
+			Arrays.stream(stage.getAltNames()).forEach(name -> stageConcatenateBuilder.append("|").append(name.toLowerCase()));
+			String stageNames = stageConcatenateBuilder.toString();
+
+			String excludeRegex = String.format("((?<=\\s)|^)not\\s+(%s)((?=\\s)|$)", stageNames);
+			salmonRunStageExcludeFilters.put(stage, Pattern.compile(excludeRegex));
+
+			String includeRegex = String.format("((?<=\\s)|^)(%s)((?=\\s)|$)", stageNames);
+			salmonRunStageIncludeFilters.put(stage, Pattern.compile(includeRegex));
 		}
 	}
 }
