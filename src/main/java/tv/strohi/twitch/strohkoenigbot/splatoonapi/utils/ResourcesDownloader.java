93e32a21595b400c997c48833e8675c09755b5b8
marco <goethe@marco-fuchs.de>
2022-06-22 00:03:58 +0200

    add additional logging to understand why this shit doesn't work

--- src/main/java/tv/strohi/twitch/strohkoenigbot/splatoonapi/utils/ResourcesDownloader.java
+++ src/main/java/tv/strohi/twitch/strohkoenigbot/splatoonapi/utils/ResourcesDownloader.java
@@ -1,5 +1,7 @@
 package tv.strohi.twitch.strohkoenigbot.splatoonapi.utils;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import tv.strohi.twitch.strohkoenigbot.chatbot.spring.DiscordBot;
@@ -14,6 +16,8 @@ import java.nio.file.Paths;
 
 @Component
 public class ResourcesDownloader {
+	private final Logger logger = LogManager.getLogger(this.getClass().getSimpleName());
+
 	private DiscordBot discordBot;
 
 	@Autowired
@@ -22,35 +26,47 @@ public class ResourcesDownloader {
 	}
 
 	public String ensureExistsLocally(String splatNetResourceUrl) {
+		logger.info("downloading a resource {}", splatNetResourceUrl);
+
 		String imageUrl = splatNetResourceUrl;
 		if (isValidURL(imageUrl)) {
 			imageUrl = imageUrl.replace("https://app.splatoon2.nintendo.net", "");
+			logger.info("new url {}", splatNetResourceUrl);
 		}
 
 		String path = Paths.get(System.getProperty("user.dir"), imageUrl).toString();
+		logger.info("path {}", path);
 
 		File file = Paths.get(path).toFile();
 		if (!file.exists() && (file.getParentFile().exists() || file.getParentFile().mkdirs())) {
+			logger.info("test 1");
 			String downloadUrl = String.format("https://app.splatoon2.nintendo.net%s", imageUrl);
 
 			try (
 					BufferedInputStream in = new BufferedInputStream(new URL(downloadUrl).openStream());
 					FileOutputStream fileOutputStream = new FileOutputStream(file.getPath())
 			) {
+				logger.info("test 2");
 				byte[] dataBuffer = new byte[1024];
 				int bytesRead;
 				while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
 					fileOutputStream.write(dataBuffer, 0, bytesRead);
 				}
 
+				logger.info("test 3");
 				String newPath = path.substring(System.getProperty("user.dir").length()).replace('\\', '/');
 				discordBot.sendServerMessageWithImages(DiscordChannelDecisionMaker.getDebugChannelName(), "I downloaded an image!", newPath);
+
+				logger.info("test 4");
 				return newPath;
 			} catch (IOException e) {
 				discordBot.sendServerMessageWithImages(DiscordChannelDecisionMaker.getDebugChannelName(), "Could not download an image because of an Exception!");
+				logger.error("exception occured!!!");
+				logger.error(e);
 				return splatNetResourceUrl;
 			}
 		} else {
+			logger.info("test 5");
 			return path.substring(System.getProperty("user.dir").length()).replace('\\', '/');
 		}
 	}
