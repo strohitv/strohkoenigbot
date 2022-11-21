28e6b435f6db3915896cb4042832fbcf71de5e48
strohitv <the.real.strohi@gmail.com>
2022-11-21 13:56:19 +0100

    use new request key hashes for regular, private and game detail

--- src/main/java/tv/strohi/twitch/strohkoenigbot/splatoon3saver/S3RequestKey.java
+++ src/main/java/tv/strohi/twitch/strohkoenigbot/splatoon3saver/S3RequestKey.java
@@ -10,10 +10,10 @@ import java.util.EnumSet;
 public enum S3RequestKey {
 	Home("dba47124d5ec3090c97ba17db5d2f4b3"),
 	Latest("7d8b560e31617e981cf7c8aa1ca13a00"),
-	Regular("819b680b0c7962b6f7dc2a777cd8c5e4"),
+	Regular("f6e7e0277e03ff14edfef3b41f70cd33"),
 	Anarchy("c1553ac75de0a3ea497cdbafaa93e95b"),
-	Private("51981299595060692440e0ca66c475a1"),
-	GameDetail("cd82f2ade8aca7687947c5f3210805a6"),
+	Private("38e0529de8bc77189504d26c7a14e0b8"),
+	GameDetail("2b085984f729cd51938fc069ceef784a"),
 	Salmon("817618ce39bcf5570f52a97d73301b30"),
 	SalmonDetail("f3799a033f0a7ad4b1b396f9a3bafb1e");
 
