9d0af61c3cc9c14fc64f8f9df7a2dc0fcd8cc14e
strohitv <the.real.strohi@gmail.com>
2024-06-04 11:56:02 +0200

    add ping action to be able to test write permissions

---
+++ src/main/java/tv/strohi/twitch/strohkoenigbot/chatbot/actions/PingAction.java
@@ -0,0 +1,33 @@
+package tv.strohi.twitch.strohkoenigbot.chatbot.actions;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import tv.strohi.twitch.strohkoenigbot.chatbot.actions.supertype.ActionArgs;
+import tv.strohi.twitch.strohkoenigbot.chatbot.actions.supertype.ArgumentKey;
+import tv.strohi.twitch.strohkoenigbot.chatbot.actions.supertype.ChatAction;
+import tv.strohi.twitch.strohkoenigbot.chatbot.actions.supertype.TriggerReason;
+
+import java.util.EnumSet;
+
+@Component
+@RequiredArgsConstructor
+public class PingAction extends ChatAction {
+	@Override
+	public EnumSet<TriggerReason> getCauses() {
+		return EnumSet.of(TriggerReason.ChatMessage, TriggerReason.DiscordMessage, TriggerReason.DiscordPrivateMessage);
+	}
+
+	@Override
+	protected void execute(ActionArgs args) {
+		String message = (String) args.getArguments().getOrDefault(ArgumentKey.Message, null);
+		if (message == null) {
+			return;
+		}
+
+		message = message.toLowerCase().trim();
+
+		if (message.startsWith("!ping")) {
+			args.getReplySender().send("pong");
+		}
+	}
+}
