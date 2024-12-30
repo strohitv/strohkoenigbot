19e9dbdd8408f8b6b319af21d9ac5d7edffcf7d8
strohitv <the.real.strohi@gmail.com>
2024-12-30 13:31:40 +0100

    feat: ✨ add rate limiting to v1/splatnet3-data
    - limit: 10 requests per minute globally

--- src/main/java/tv/strohi/twitch/strohkoenigbot/rest/SplatNet3DataController.java
+++ src/main/java/tv/strohi/twitch/strohkoenigbot/rest/SplatNet3DataController.java
@@ -1,11 +1,15 @@
 package tv.strohi.twitch.strohkoenigbot.rest;
 
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
@@ -20,6 +24,7 @@ public class SplatNet3DataController {
 	private final String REFRESH_KEY = "_last_refresh";
 
 	private final Map<String, Object> data = new HashMap<>();
+	private final Bucket bucket;
 
 	public SplatNet3DataController() {
 		var now = Instant.now().toEpochMilli();
@@ -29,6 +34,15 @@ public class SplatNet3DataController {
 
 		data.put(STORE_KEY, Map.of());
 		data.put(String.format("%s%s", STORE_KEY, REFRESH_KEY), now);
+
+		var limit = Bandwidth.builder()
+			.capacity(10)
+			.refillGreedy(10, Duration.ofMinutes(1))
+			.build();
+
+		this.bucket = Bucket.builder()
+			.addLimit(limit)
+			.build();
 	}
 
 	public void refresh(String key, Object newValue) {
@@ -41,7 +55,11 @@ public class SplatNet3DataController {
 	}
 
 	@GetMapping(value = "splatnet3-data", produces = "application/json")
-	public @ResponseBody Map<String, Object> getS3Overlay() {
-		return data;
+	public ResponseEntity<Map<String, Object>> getS3Overlay() {
+		if (bucket.tryConsume(1)) {
+			return ResponseEntity.ok(data);
+		}
+
+		return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
 	}
 }
