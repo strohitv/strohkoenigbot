64ac3e1a9e57f11a1ba166f1a73383750b46bf7c
strohitv <the.real.strohi@gmail.com>
2025-01-02 13:01:20 +0100

    feat: ✨ add admin message sender web request to BotController
    - add rate limited POST /bot/admin-message endpoint which can be used by other applications to send a discord private message to the bot admin.
    - can be easily curled and thus is useful in many different use cases

--- src/main/java/tv/strohi/twitch/strohkoenigbot/rest/BotController.java
+++ src/main/java/tv/strohi/twitch/strohkoenigbot/rest/BotController.java
@@ -1,19 +1,35 @@
 package tv.strohi.twitch.strohkoenigbot.rest;
 
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import tv.strohi.twitch.strohkoenigbot.chatbot.TwitchBotClient;
+import tv.strohi.twitch.strohkoenigbot.chatbot.spring.DiscordBot;
 import tv.strohi.twitch.strohkoenigbot.data.model.Account;
 import tv.strohi.twitch.strohkoenigbot.data.repository.AccountRepository;
 import tv.strohi.twitch.strohkoenigbot.rest.model.BotStatus;
 import tv.strohi.twitch.strohkoenigbot.splatoonapi.results.ResultsExporter;
+import tv.strohi.twitch.strohkoenigbot.utils.ComputerNameEvaluator;
+import tv.strohi.twitch.strohkoenigbot.utils.DiscordChannelDecisionMaker;
+
+import java.time.Duration;
 
 @RestController
 @RequestMapping("/bot")
 public class BotController {
+	private final Bucket bucket;
+
+	private DiscordBot discordBot;
+
+	@Autowired
+	public void setDiscordBot(DiscordBot discordBot) {
+		this.discordBot = discordBot;
+	}
+
 	private TwitchBotClient twitchBotClient;
 
 	@Autowired
@@ -35,14 +51,25 @@ public class BotController {
 		this.accountRepository = accountRepository;
 	}
 
+	public BotController() {
+		var limit = Bandwidth.builder()
+			.capacity(10)
+			.refillGreedy(10, Duration.ofMinutes(1))
+			.build();
+
+		this.bucket = Bucket.builder()
+			.addLimit(limit)
+			.build();
+	}
+
 	@GetMapping()
 	public BotStatus getBotStatus() {
 		BotStatus status = new BotStatus();
 
 		Account account = accountRepository.findAll().stream()
-				.filter(a -> a.getIsMainAccount() != null && a.getIsMainAccount())
-				.findFirst()
-				.orElse(new Account());
+			.filter(a -> a.getIsMainAccount() != null && a.getIsMainAccount())
+			.findFirst()
+			.orElse(new Account());
 		status.setRunning(twitchBotClient.isLive(account.getTwitchUserId()));
 
 		return status;
@@ -51,9 +78,9 @@ public class BotController {
 	@PostMapping("start")
 	public void startExporter() {
 		Account account = accountRepository.findAll().stream()
-				.filter(a -> a.getIsMainAccount() != null && a.getIsMainAccount())
-				.findFirst()
-				.orElse(new Account());
+			.filter(a -> a.getIsMainAccount() != null && a.getIsMainAccount())
+			.findFirst()
+			.orElse(new Account());
 
 		if (!twitchBotClient.isLive(account.getTwitchUserId())) {
 			twitchBotClient.setFakeDebug(true);
@@ -64,13 +91,27 @@ public class BotController {
 	@PostMapping("stop")
 	public void stopExporter() {
 		Account account = accountRepository.findAll().stream()
-				.filter(a -> a.getIsMainAccount() != null && a.getIsMainAccount())
-				.findFirst()
-				.orElse(new Account());
+			.filter(a -> a.getIsMainAccount() != null && a.getIsMainAccount())
+			.findFirst()
+			.orElse(new Account());
 
 		if (twitchBotClient.isLive(account.getTwitchUserId())) {
 			twitchBotClient.setFakeDebug(false);
 			resultsExporter.stop(account);
 		}
+	}
+
+	@PostMapping("admin-message")
+	public ResponseEntity<Void> sendAdminMessage(@RequestBody @NonNull String message) {
+		if (bucket.tryConsume(1)) {
+			if (message.length() <= 1800) {
+				discordBot.sendPrivateMessage(DiscordBot.ADMIN_ID, String.format("Instance `%s`, debug = `%s` received message via web interface:\n```\n%s\n```", ComputerNameEvaluator.getComputerName(), DiscordChannelDecisionMaker.isLocalDebug(), message));
+				return ResponseEntity.ok().build();
+			} else {
+				ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+			}
+		}
+
+		return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
 	}
 }
