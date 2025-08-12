a673373bb2c721e0485d0a69610ba1369319cab9
strohitv <the.real.strohi@gmail.com>
2025-08-12 11:38:36 +0200

    feat: ✨ store Beelink activity messages in database and only notify on discord whenever the bot does not receive messages from Beelink in a span of 6 hours.

--- src/main/java/tv/strohi/twitch/strohkoenigbot/rest/BotController.java
+++ src/main/java/tv/strohi/twitch/strohkoenigbot/rest/BotController.java
@@ -3,55 +3,49 @@ package tv.strohi.twitch.strohkoenigbot.rest;
 import io.github.bucket4j.Bandwidth;
 import io.github.bucket4j.Bucket;
 import lombok.NonNull;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tv.strohi.twitch.strohkoenigbot.chatbot.TwitchBotClient;
 import tv.strohi.twitch.strohkoenigbot.chatbot.spring.DiscordBot;
 import tv.strohi.twitch.strohkoenigbot.data.model.Account;
+import tv.strohi.twitch.strohkoenigbot.data.model.Configuration;
 import tv.strohi.twitch.strohkoenigbot.data.repository.AccountRepository;
+import tv.strohi.twitch.strohkoenigbot.data.repository.ConfigurationRepository;
 import tv.strohi.twitch.strohkoenigbot.rest.model.BotStatus;
 import tv.strohi.twitch.strohkoenigbot.splatoonapi.results.ResultsExporter;
 import tv.strohi.twitch.strohkoenigbot.utils.ComputerNameEvaluator;
 import tv.strohi.twitch.strohkoenigbot.utils.DiscordChannelDecisionMaker;
+import tv.strohi.twitch.strohkoenigbot.utils.scheduling.ScheduledService;
+import tv.strohi.twitch.strohkoenigbot.utils.scheduling.model.ScheduleRequest;
+import tv.strohi.twitch.strohkoenigbot.utils.scheduling.model.TickSchedule;
 
 import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 @RestController
 @RequestMapping("/bot")
-public class BotController {
+public class BotController implements ScheduledService {
+	private final String BEELINK_BOOT_MESSAGE_CONFIG = "BotController_BeelinkMessageTime";
+
 	private final Bucket bucket;
 
-	private DiscordBot discordBot;
+	private final DiscordBot discordBot;
+	private final TwitchBotClient twitchBotClient;
+	private final ResultsExporter resultsExporter;
 
-	@Autowired
-	public void setDiscordBot(DiscordBot discordBot) {
+	private final AccountRepository accountRepository;
+	private final ConfigurationRepository configurationRepository;
+
+	public BotController(DiscordBot discordBot, TwitchBotClient twitchBotClient, ResultsExporter resultsExporter, AccountRepository accountRepository, ConfigurationRepository configurationRepository) {
 		this.discordBot = discordBot;
-	}
-
-	private TwitchBotClient twitchBotClient;
-
-	@Autowired
-	public void setTwitchBotClient(TwitchBotClient twitchBotClient) {
 		this.twitchBotClient = twitchBotClient;
-	}
-
-	private ResultsExporter resultsExporter;
-
-	@Autowired
-	public void setResultsExporter(ResultsExporter resultsExporter) {
 		this.resultsExporter = resultsExporter;
-	}
-
-	private AccountRepository accountRepository;
-
-	@Autowired
-	public void setAccountRepository(AccountRepository accountRepository) {
 		this.accountRepository = accountRepository;
-	}
+		this.configurationRepository = configurationRepository;
 
-	public BotController() {
 		var limit = Bandwidth.builder()
 			.capacity(10)
 			.refillGreedy(10, Duration.ofMinutes(1))
@@ -103,15 +97,55 @@ public class BotController {
 
 	@PostMapping("admin-message")
 	public ResponseEntity<Void> sendAdminMessage(@RequestBody @NonNull String message) {
-		if (bucket.tryConsume(1)) {
-			if (message.length() <= 1800) {
-				discordBot.sendPrivateMessage(DiscordBot.ADMIN_ID, String.format("Instance `%s`, debug = `%s` received message via web interface:\n```\n%s\n```", ComputerNameEvaluator.getComputerName(), DiscordChannelDecisionMaker.isLocalDebug(), message));
-				return ResponseEntity.ok().build();
-			} else {
-				ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-			}
+		if (!bucket.tryConsume(1)) {
+			return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
 		}
 
-		return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+		if (message.length() > 1800) {
+			ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+
+		if ("Beelink booted successfully.".equals(message)) {
+			var config = configurationRepository.findByConfigName(BEELINK_BOOT_MESSAGE_CONFIG)
+				.orElseGet(() -> configurationRepository.save(Configuration.builder()
+					.configName(BEELINK_BOOT_MESSAGE_CONFIG)
+					.configValue(String.format("%d", Instant.now().getEpochSecond()))
+					.build()));
+
+			config.setConfigValue(String.format("%d", Instant.now().getEpochSecond()));
+			configurationRepository.save(config);
+		} else {
+			discordBot.sendPrivateMessage(DiscordBot.ADMIN_ID, String.format("Instance `%s`, debug = `%s` received message via web interface:\n```\n%s\n```", ComputerNameEvaluator.getComputerName(), DiscordChannelDecisionMaker.isLocalDebug(), message));
+		}
+
+		return ResponseEntity.ok().build();
+	}
+
+	private void checkBeelinkActivity() {
+		var config = configurationRepository.findByConfigName(BEELINK_BOOT_MESSAGE_CONFIG)
+			.orElseGet(() -> configurationRepository.save(Configuration.builder()
+				.configName(BEELINK_BOOT_MESSAGE_CONFIG)
+				.configValue(String.format("%d", Instant.now().getEpochSecond()))
+				.build()));
+
+		var lastMessageTimestamp = Instant.ofEpochSecond(Long.parseLong(config.getConfigValue()));
+
+		if (Instant.now().isAfter(lastMessageTimestamp.plus(6, ChronoUnit.HOURS))) {
+			discordBot.sendPrivateMessage(DiscordBot.ADMIN_ID, String.format("## WARNING\nInstance `%s`, debug = `%s` did **not** receive a message from Beelink for 6 hours! Last message EpochSecond: `%s`", ComputerNameEvaluator.getComputerName(), DiscordChannelDecisionMaker.isLocalDebug(), config.getConfigValue()));
+		}
+	}
+
+	@Override
+	public List<ScheduleRequest> createScheduleRequests() {
+		return List.of(ScheduleRequest.builder()
+			.name("BotController_checkBeelinkTiming")
+			.schedule(TickSchedule.getScheduleString(TickSchedule.everyHours(6)))
+			.runnable(this::checkBeelinkActivity)
+			.build());
+	}
+
+	@Override
+	public List<ScheduleRequest> createSingleRunRequests() {
+		return List.of();
 	}
 }
