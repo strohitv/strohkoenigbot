4e8c91327aaeabe23b188d352f11c5463f54d978
strohitv <the.real.strohi@gmail.com>
2026-02-11 10:38:55 +0100

    chore: 🚚 rename FrontendController to FrontendRedirectController

--- src/main/java/tv/strohi/twitch/strohkoenigbot/rest/FrontendRedirectController.java
+++ src/main/java/tv/strohi/twitch/strohkoenigbot/rest/FrontendRedirectController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
-public class FrontendController {
+public class FrontendRedirectController {
 	@GetMapping("/frontend/{path:[^.]*}")
 	public String forward(@PathVariable(value = "path", required = false) String path) {
 		return "forward:/frontend/index.html";
